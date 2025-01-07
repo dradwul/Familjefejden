23d8ca549a3a28d84b70ce66e589bf82cb835679
Marcus <145341135+mlundberg93@users.noreply.github.com>
2025-01-07 14:16:46 +0100

    Lagt till dummy resultat för lika i resultat fliken

--- Familjefejden/DummyData.cs
+++ Familjefejden/DummyData.cs
@@ -35,6 +35,7 @@ namespace Familjefejden
                 new Match { Id = 1, HemmalagId = 1, BortalagId = 2, HemmalagMal = 2, BortalagMal = 1, Date = new DateTime(2024, 12, 28, 19, 0, 0) },
                 new Match { Id = 2, HemmalagId = 3, BortalagId = 4, HemmalagMal = 4, BortalagMal = 2, Date = new DateTime(2024, 12, 26, 19, 0, 0) },
                 new Match { Id = 3, HemmalagId = 5, BortalagId = 6, HemmalagMal = 7, BortalagMal = 4, Date = new DateTime(2025, 1, 1, 19, 0, 0) },
+                new Match { Id = 4, HemmalagId = 10, BortalagId = 2, HemmalagMal = 7, BortalagMal = 7, Date = new DateTime(2025, 1, 3, 01, 30, 0) },
 
             };
         }
