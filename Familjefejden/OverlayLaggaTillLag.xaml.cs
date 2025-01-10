4ce068527ab923da47510c44ec15cef457af04e2
Ludvig Andersson <mlt.andersson@outlook.com>
2025-01-10 17:28:05 +0100

    hämtar flaggadresser från separat klass istället

--- Familjefejden/OverlayLaggaTillLag.xaml.cs
+++ Familjefejden/OverlayLaggaTillLag.xaml.cs
@@ -39,21 +39,7 @@ namespace Familjefejden
 
         private async void OverlayLaggaTillLag_Loaded(object sender, RoutedEventArgs e)
         {
-            var bildText = new List<LagForemal>
-            {
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Canada.png", Lag = "Kanada" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Finland.png", Lag = "Finland" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Czech_Republic.png", Lag = "Tjeckien" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Germany.png", Lag = "Tyskland" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Kazakhstan.png", Lag = "Kazakhstan" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Latvia.png", Lag = "Lettland" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Slovakia.png", Lag = "Slovakien" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Sweden.png", Lag = "Sverige" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Switzerland.png", Lag = "Schweiz" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/USA.png", Lag = "USA" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Denmark.png", Lag = "Danmark" },
-                new LagForemal { LagFlagga = "ms-appx:///Assets/Austria.png", Lag = "Österrike" }
-            };
+            var bildText = new List<LagForemal>(LagForemal.HamtaLagForemal());
             LagLista.ItemsSource = bildText;
 
             TurneringNamn.Text = await jsonService.HamtaTurneringsnamnFranId(1);
